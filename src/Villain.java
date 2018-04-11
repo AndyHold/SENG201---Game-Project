d042851196089c379960d00dbdc436b937b4c865
Andrew Holden <ash102@uclive.ac.nz>
2018-04-11 13:59:25 +1200

    Created Villains Class, changed Villain to an enum, deleted all subclasses of Villain. Updated UML

--- src/Villain.java
+++ src/Villain.java
@@ -1,14 +1,40 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+
 /**
- * Villain Class for Heroes & Villains Game
+ * Villain Enum for Heroes & Villains Game
  * SENG201 2018S1
  * @author Andy Holden & Alex Liggett
  */
-public class Villain {
+public enum Villain {
+	
+	
+	POLITICIAN("Peter the Politician", 
+			"The people want what I say they want!", 
+			"Let's mine the national parks for oil!", 
+			"Election promises are for losers!"), 
+	LAWYER("Lionel the Lawyer", 
+			"I object to your presence!", 
+			"I'm going to take you for every penny you have!", 
+			"You're contractually obliged to lose!"), 
+	BOUNCER("Dan the Doorman", 
+			"Sorry cuz, you've had a bit much to come in tonight!", 
+			"This isn't your I.D.!", 
+			"You look like you need a few waters!"), 
+	CALLCENTREOPERATOR("Carol the Call Centre Operator", 
+			"I'll put you on hold for the rest of your life!", 
+			"Your call is not important to us!", 
+			"Let me put you through to our complaints department, Oh wait we don't have one!"), 
+	BADRUGBYREFEREE("Barry the Bad Referee", 
+			"I'm going to penalize you off the paddock!", 
+			"Do you even know the rules?!", 
+			"Thats a red card for you buddy!");
 	
 	private String name;
 	private int currentHealth;
 	private boolean alive = true;
-	
+	private ArrayList<String> taunts = new ArrayList<String>();
 	
 	
 	/**
@@ -16,18 +42,34 @@ public class Villain {
 	 * @param newName name of the Villain
 	 * @param currentHealth health of the Villain
 	 */
-	public Villain(String newName) {
+	private Villain(String newName, String taunt1, String taunt2, String taunt3) {
 		name = newName;
 		currentHealth = 100;
+		setTaunts(taunt1, taunt2, taunt3);
 	}
 	
 	
 	/**
-	 * Name of the villain
-	 * @return String name of the Villain
+	 * Fills the arrayList taunts with three taunts specific to the Villain type
+	 * @param taunts1 first taunt specific to the Villain type
+	 * @param taunts2 second taunt specific to the Villain type
+	 * @param taunts3 third taunt specific to the Villain type
 	 */
-	public String getName() {
-		return name;
+	private void setTaunts(String taunt1, String taunt2, String taunt3){
+		taunts.add(taunt1);
+		taunts.add(taunt2);
+		taunts.add(taunt3);
+	}
+	
+	
+	/**
+	 * Randomly picks a taunt from the arrayList taunts
+	 * @return a random String taunt from the arrayList taunts
+	 */
+	public String getTaunt() {
+		Random rand = new Random();
+		int n = rand.nextInt(3);
+		return taunts.get(n);
 	}
 	
 	
@@ -64,12 +106,18 @@ public class Villain {
 	}
 	
 	
+	@Override
+	public String toString() {
+		return name + " says: \n" + getTaunt();
+	}
+	
+	
 	/**
 	 * Test code only
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-//		Villain baddie = new Villain();
+//		Villain baddie = new Villain.POLITICIAN;
 //		baddie.getTaunt();
 //		System.out.println(barister);
 //		System.out.println(barister);
---
+++ src/Villains.java
@@ -0,0 +1,77 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+
+/**
+ * Villains Class for Heroes & Villains Game
+ * SENG201 2018S1
+ * @author Andy Holden & Alex Liggett
+ */
+public class Villains {
+	
+	
+	private static ArrayList<Villain> availableVillains = new ArrayList<Villain>();
+	
+	
+	/**
+	 * Constructor for Villains Super Class
+	 * @param availableVillains ArrayList<Villain> list of all available villains
+	 */
+	private Villains() {
+		availableVillains.add(Villain.BADRUGBYREFEREE);
+		availableVillains.add(Villain.BOUNCER);
+		availableVillains.add(Villain.CALLCENTREOPERATOR);
+		availableVillains.add(Villain.LAWYER);
+		availableVillains.add(Villain.POLITICIAN);
+	}
+	
+	
+	/**
+	 * Gets the number of Villains currently not in use.
+	 * @return int number of Villains not in use. 
+	 */
+	public static int getNumberOfVillains() {
+		return availableVillains.size();
+	}
+	
+	
+	/**
+	 * Chooses a random villain for use and removes it from availableVillains
+	 * @return Villain a random villain picked from availableVillains.
+	 */
+	public static Villain getVillain() {
+		Random rand = new Random();
+		int n = rand.nextInt(getNumberOfVillains());
+		Villain currentVillain = availableVillains.get(n);
+		availableVillains.remove(n);
+		return currentVillain;
+	}
+	
+	
+	@Override
+	public String toString() {
+		String result = "The available Villains are: \n";
+		for(Villain badguy: availableVillains) {
+			result += (badguy + "\n");
+		}
+		return result;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		Villains baddies = new Villains();
+//		System.out.println(baddies);
+//		Villain baddie1 = Villains.getVillain();
+//		System.out.println(baddie1);
+//		Villain baddie2 = Villains.getVillain();
+//		System.out.println(baddie2);
+//		Villain baddie3 = Villains.getVillain();
+//		System.out.println(baddie3);
+//		Villain baddie4 = Villains.getVillain();
+//		System.out.println(baddie4);
+//		Villain baddie5 = Villains.getVillain();
+//		System.out.println(baddie5);
+		// GET AN ERROR WHEN TRYING TO PRINT AN EMPTY VILLAINS!!!
+	}
+
+}
