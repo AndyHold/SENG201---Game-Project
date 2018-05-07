a4e58102d072a4c24381f101f340ae7887795560
Andrew Holden <ash102@uclive.ac.nz>
2018-05-07 12:21:34 +1200

    Did Javadoc for LocationType

--- src/HeroGame/LocationType.java
+++ src/HeroGame/LocationType.java
@@ -1,5 +1,11 @@
 package HeroGame;
 
+
+/**
+ * LocationType Enum for Heroes & Villains Game
+ * SENG201 2018S1
+ * @author Andy Holden & Alex Liggett
+ */
 public enum LocationType {
 	
 	HOMEBASE, SHOP, VILLIANSLAIR, HOSPITAL, POWERUPDEN
